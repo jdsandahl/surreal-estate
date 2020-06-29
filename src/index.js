abfe7c34af203fc2ccdf396a7128caf609bfd1ab
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-29 20:42:12 +0100

    added Google Fonts

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
+ReactDOM.render( 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
