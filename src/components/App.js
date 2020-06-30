ed9ffe9eed64898339d9c8aeb9c204159c461f49
Justin Sandahl <sandahl.j.d@gmail.com>
2020-06-30 17:16:16 +0100

    added react-router-dom as a dependency and imported into App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "../styles/App.css";
-import NavBar from './NavBar.js'
+import { BrowserRouter } from "react-router-dom";
+import NavBar from './NavBar.js';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <NavBar />
     </div>
+    </BrowserRouter>
   );
 }
 
