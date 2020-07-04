30c8f1aecb50c3aecebc8242bab5874a0ea2e8f1
Justin Sandahl <sandahl.j.d@gmail.com>
2020-07-04 14:12:01 +0100

    added mock values to PropertyForm test

--- src/__tests__/PropertyForm.test.js
+++ src/__tests__/PropertyForm.test.js
@@ -4,15 +4,51 @@ import { render, fireEvent } from "@testing-library/react";
 import PropertyForm from "../components/PropertyForm";
 
 describe("PropertyForm", () => {
+  const mockHandleFieldChange = jest.fn();
+  const mockHandleAddProperty = jest.fn();
+  const mockData = {
+    title: "",
+    city: "Manchester",
+    type: "Flat",
+    bedrooms: "2",
+    bathrooms: "1",
+    price: 100000,
+    email: "test@test.com",
+  };
+
   test("the page renders correctly against snapshot", () => {
-    const { asFragment } = render(<PropertyForm />);
+    const { asFragment } = render(
+      <PropertyForm
+        handleAddProperty={mockHandleAddProperty}
+        handleFieldChange={mockHandleFieldChange}
+        title={mockData.title}
+        city={mockData.city}
+        type={mockData.type}
+        bedrooms={mockData.bedrooms}
+        bathrooms={mockData.bathrooms}
+        price={mockData.price}
+        email={mockData.email}
+      />
+    );
     const component = asFragment();
 
     expect(component).toMatchSnapshot();
   });
 
   test("renders AddProperty component", () => {
-    const { getByRole, getByLabelText } = render(<PropertyForm />);
+    const { getByRole, getByLabelText } = render(
+      <PropertyForm
+        handleAddProperty={mockHandleAddProperty}
+        handleFieldChange={mockHandleFieldChange}
+        title={mockData.title}
+        city={mockData.city}
+        type={mockData.type}
+        bedrooms={mockData.bedrooms}
+        bathrooms={mockData.bathrooms}
+        price={mockData.price}
+        email={mockData.email}
+      />
+    );
 
     expect(getByRole("button")).toHaveTextContent(/add/i);
     expect(getByLabelText(/title/i)).toBeInTheDocument();
