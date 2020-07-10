ee587d49c368194b013c347a42621d323b89fbcc
Justin Sandahl <sandahl.j.d@gmail.com>
2020-07-10 19:45:09 +0100

    refactored PropertyListings components to use ...property spread operator instead of declaring each prop explicitly

--- src/components/PropertyListings.js
+++ src/components/PropertyListings.js
@@ -6,23 +6,14 @@ import PropertyCard from "./PropertyCard";
 
 const PropertyListings = ({ listings }) => (
   <div className="property-listings">
-    { listings.map((property) => (
-      <PropertyCard
-        key={property._id}
-        title={property.title}
-        type={property.type}
-        city={property.city}
-        bedrooms={property.bedrooms}
-        bathrooms={property.bathrooms}
-        price={property.price}
-        email={property.email}
-      />
+    {listings.map((property) => (
+      <PropertyCard key={property._id} {...property} />
     ))}
   </div>
 );
 
 PropertyListings.propTypes = {
-    listings: PropTypes.array.isRequired,
-  };
+  listings: PropTypes.array.isRequired,
+};
 
 export default PropertyListings;
