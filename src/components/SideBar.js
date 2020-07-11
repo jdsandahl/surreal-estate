ca7aebcb082122d719bcfe10cfb26e67872728eb
Justin Sandahl <sandahl.j.d@gmail.com>
2020-07-11 12:12:44 +0100

    added the a query Link in the Sidebar component to allow a filter to show all cities, basically a filter reset.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,21 +5,44 @@ import "../styles/SideBar.css";
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <label>Filter By City:  
-      <ul className="sidebar__links">
-        <li className="sidebar__item">
-          <Link to={`/?query={"city": "Manchester"}`} className="sidebar__link">Manchester</Link>
-        </li>
-        <li className="sidebar__item">
-          <Link to={`/?query={"city": "Leeds"}`} className="sidebar__link">Leeds</Link>
-        </li>
-        <li className="sidebar__item">
-          <Link to={`/?query={"city": "Sheffield"}`} className="sidebar__link">Sheffield</Link>
-        </li>
-        <li className="sidebar__item">
-          <Link to={`/?query={"city": "Liverpool"}`} className="sidebar__link">Liverpool</Link>
-        </li>
-      </ul>
+      <label className="sidebar_section">
+        Filter By City:
+        <ul className="sidebar__links">
+          <li className="sidebar__item">
+            <Link to={`/`} className="sidebar__link">
+              All
+            </Link>
+          </li>
+          <li className="sidebar__item">
+            <Link
+              to={`/?query={"city": "Manchester"}`}
+              className="sidebar__link"
+            >
+              Manchester
+            </Link>
+          </li>
+          <li className="sidebar__item">
+            <Link to={`/?query={"city": "Leeds"}`} className="sidebar__link">
+              Leeds
+            </Link>
+          </li>
+          <li className="sidebar__item">
+            <Link
+              to={`/?query={"city": "Sheffield"}`}
+              className="sidebar__link"
+            >
+              Sheffield
+            </Link>
+          </li>
+          <li className="sidebar__item">
+            <Link
+              to={`/?query={"city": "Liverpool"}`}
+              className="sidebar__link"
+            >
+              Liverpool
+            </Link>
+          </li>
+        </ul>
       </label>
     </div>
   );
