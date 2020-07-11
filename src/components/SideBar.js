8920cee9b5d7e034ee2be8493ce2b5b5d6dbc0b7
Justin Sandahl <sandahl.j.d@gmail.com>
2020-07-11 15:50:07 +0100

    added qs as a dependency and created a buildQueryString method that uses data from useLocation() to create the queries used in in the SideBar component to filter results by city and to also sort results by price.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import qs from "qs";
 import "../styles/SideBar.css";
 
 const SideBar = () => {
+  const { search } = useLocation();
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify(valueObj),
+    };
+
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
   return (
     <div className="sidebar">
       <label className="sidebar_section">
@@ -15,20 +30,23 @@ const SideBar = () => {
           </li>
           <li className="sidebar__item">
             <Link
-              to={`/?query={"city": "Manchester"}`}
+              to={buildQueryString("query", { city: "Manchester" })}
               className="sidebar__link"
             >
               Manchester
             </Link>
           </li>
           <li className="sidebar__item">
-            <Link to={`/?query={"city": "Leeds"}`} className="sidebar__link">
+            <Link
+              to={buildQueryString("query", { city: "Leeds" })}
+              className="sidebar__link"
+            >
               Leeds
             </Link>
           </li>
           <li className="sidebar__item">
             <Link
-              to={`/?query={"city": "Sheffield"}`}
+              to={buildQueryString("query", { city: "Sheffield" })}
               className="sidebar__link"
             >
               Sheffield
@@ -36,10 +54,31 @@ const SideBar = () => {
           </li>
           <li className="sidebar__item">
             <Link
-              to={`/?query={"city": "Liverpool"}`}
+              to={buildQueryString("query", { city: "Liverpool" })}
               className="sidebar__link"
             >
               Liverpool
+            </Link>
+          </li>
+        </ul>
+      </label>
+      <label className="sidebar__section">
+        Sort By:
+        <ul className="sidebar__links">
+          <li className="sidebar__item">
+            <Link
+              to={buildQueryString("sort", { price: 1 })}
+              className="sidebar__link"
+            >
+              Price - Ascending
+            </Link>
+          </li>
+          <li className="sidebar__item">
+            <Link
+              to={buildQueryString("sort", { price: -1 })}
+              className="sidebar__link"
+            >
+              Price - Descending
             </Link>
           </li>
         </ul>
