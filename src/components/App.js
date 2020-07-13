3526fc8e816bf13475c2a20f5b0444775842e8e3
Justin Sandahl <sandahl.j.d@gmail.com>
2020-07-13 18:11:05 +0100

    attempting to add Facebook login

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar.js";
@@ -6,13 +6,20 @@ import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 
 function App() {
+  const [userId, setUserId] = useState("");
+
+  const handleLogin = (response) => {
+    console.log(response.data);
+  };
+
   return (
     <div className="App">
-      <NavBar />
+      <NavBar onLogin={handleLogin} />
       <Switch>
         <Route exact path="/" component={Properties} />
         <Route exact path="/add-property" component={AddProperty} />
       </Switch>
+      <div id="fb-root"></div>
     </div>
   );
 }
